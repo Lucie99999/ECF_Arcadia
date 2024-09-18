57cc7684bcc9a95ab43ee334c8cfbf59a14314d9
Lucie99999 <lucie.saurel3@orange.fr>
2024-09-18 11:39:25 +0200

    change in nosql connection

--- config/noSQL.php
+++ config/noSQL.php
@@ -3,9 +3,18 @@
 //Chargement de l'autoload de Composer
 require 'vendor/autoload.php';
 
+use MongoDB\Driver\ServerApi;
+
 try {
+
+    //Définition de l'uri pour se connecter à MongoDB
+    $uri = 'mongodb+srv://luciesaurel3:xiDgej-vidwoq-wardi8@ecfarcadiastudi.aabmp.mongodb.net/?retryWrites=true&w=majority&appName=ECFArcadiaStudi';
+
+    // Set the version of the Stable API on the client
+    $apiVersion = new ServerApi(ServerApi::V1);
+
     //Utilisation du client MongoDB pour se connecter à la base noSQL
-    $client = new MongoDB\Client("mongodb+srv://luciesaurel3:xiDgej-vidwoq-wardi8@ecfarcadiastudi.aabmp.mongodb.net/?retryWrites=true&w=majority&appName=ECFArcadiaStudi");
+    $client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
 
     // Sélection de la base de données
     $db = $client->selectDatabase("ECFArcadia");
